4e85a0ab5c858d322a33ef10ceeaa652ab9416ad
Sean-2002 <Sean2002@users.noreply.github.com>
2018-02-12 21:15:57 -0800

    Mailgun with mongodb

--- mailgun/index.js
+++ mailgun/index.js
@@ -1,15 +1,44 @@
-const fs = require("fs");
-var api_key = fs.readFileSync('C:\\MailgunApiKey.txt');
-var domain = 'seansun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+//vars
+var key; //the mailgun api key
+const from = 'Sean <no-reply@seansun.org>';
+const to = 'sean.sun@sonomaacademy.org';
+const subject = 'Email from seansun.org';
+const text = 'Hello World!';
+const domain = 'seansun.org';
+//require libraries
+const {MongoClient,ObjectID} = require("mongodb");
+var mailgun;
 
-var data = {
-    from: 'Sean <no-reply@seansun.org>',
-    to: 'sean.sun@sonomaacademy.org',
-    subject: 'Email from seansun.org',
-    text: 'Hello World!'
-};
-
-mailgun.messages().send(data, function (error, body) {
-    console.log(body);
+//get the mailgun api key: under mongo database -> MailgunApiKey -> key -> key
+MongoClient.connect("mongodb://localhost:27017/MailgunApiKey", (err, db) => {
+    if (err)
+        return console.log("Unable to connect to MongoDB server", err);
+    // console.log("Connected to MongoDB server");
+    //connected to database
+    db.collection("key").find().toArray().then((docs)=>{
+        key = docs[0].key;
+        // console.log(docs[0].key);
+        sendEmail();//got key -> send email now
+    },(err)=>{//got error -> console and exit
+        console.log("Unable to get mailgun api key ",err);
+    });
+    db.close(); //close database
 });
+
+//not going to save it in a txt anymore
+// const fs = require("fs");
+// var api_key = fs.readFileSync('C:\\MailgunApiKey.txt');
+
+function sendEmail() {
+    mailgun = require('mailgun-js')({apiKey: key, domain: domain});
+    var data = {
+        from: from,
+        to: to,
+        subject: subject,
+        text: text
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        console.log(body);
+    });
+}
